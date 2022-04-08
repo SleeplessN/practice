c7ceb313b541447159f27d5cea100e809a06c748
SleeplessN <102517673+SleeplessN@users.noreply.github.com>
2022-04-08 18:02:11 +0900

    color applied as library jQuery

--- colors.js
+++ colors.js
@@ -1,19 +1,22 @@
 var Body = {
   setColor : function(color){
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   } ,
   setBackgroundColor : function(color){
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 }
 var Links = {
   setColor : function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i<alist.length){
-        alist[i].style.color = color;
-        i++;
-    }
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i<alist.length){
+    //     alist[i].style.color = color;
+    //     i++;
+    // }
+    $('a').css('color', color);
   }
 }
 function NightDayHandler(self){
